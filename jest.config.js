7a6f907a2d1417321679a6668c929f2fdf0f23c9
wattanx <wattan.dev@gmail.com>
2021-09-11 01:06:38 +0900

    Jest setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+};
---
+++ src/__tests__/test-setup.test.tsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom';
+import { Hero } from 'components/Hero';
+
+test('Test Setup', () => {
+  const id = '1';
+  expect(id).toBe('1');
+});
